d5fc6e9980088d3696060e624b1e05b984735429
4pd4stantone <victor.stanton@gmail.com>
2025-11-11 15:24:42 -0500

    Created the task completed state so that when the checkbox is check the delete button is activated

--- todolist-usereducelab/src/List.jsx
+++ todolist-usereducelab/src/List.jsx
@@ -1,19 +1,27 @@
+import { useState } from 'react'
 
 export default function List({toDoList, setTodoList}) {
 
+        const [taskCompleted, setTaskCompleted] = useState(true)
 
-        const listOfToDoItems = toDoList.map(task => (
-            <div key={task} className="list">
-                <input type="checkbox" name="done" value="done" />
-                <div className='task-area'>{task}</div>
-                <button>Edit</button>
-                <button disabled={true}>Delete</button>
-            </div>
-        ))
+        function handleCheckBox(e) {
+            setTaskCompleted(!taskCompleted);
+            console.log(taskCompleted)
+        }
 
     return (
         <section id='todolist-section' >
-            <ul className="todolist" >{listOfToDoItems}</ul>
+            <ul className="todolist" >
+                {toDoList.map(task => (
+                        <div key={task} className="list">
+                            <input type="checkbox" name="checkbox" onChange={handleCheckBox}/>
+                            <div className='task-area'>{task}</div>
+                            <button>Edit</button>
+                            <button disabled={taskCompleted}>Delete</button>
+                        </div>
+                    ))
+                }
+            </ul>
         </section>
     )
 
