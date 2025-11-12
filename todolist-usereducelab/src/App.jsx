e1fb0238eeb00a90ea3d0755a70d4ff57a0ee110
4pd4stantone <victor.stanton@gmail.com>
2025-11-12 00:06:21 -0500

    added some styling

--- todolist-usereducelab/src/App.jsx
+++ todolist-usereducelab/src/App.jsx
@@ -1,10 +1,14 @@
 import "./styles.css";
 import Form from "./Form.jsx";
+import logo from "./assets/todolist-logo.png"
 
 export default function App() {
   return (
     <div className="App">
-      <h1>To-do List App</h1>
+      <header>
+        <img src={logo} alt="to do list logo" id="logo"/>
+        <h1 id="title">To-Do List App</h1>
+      </header>
       <Form />
     </div>
   );
